09af8fd0595e0886ac0e7cacbcf3c8dcea68c8af
김준혁 <skdhd999@gmail.com>
2024-05-10 17:33:18 +0900

    Board 테스트 테이터 생성문

---
+++ board/sql/BoardTestData.sql
@@ -0,0 +1,5 @@
+INSERT INTO board (member_id, title, content, reg_date, views, update_date) VALUES (1, 'title 1', 'content 1', sysdate, 1, sysdate);
+INSERT INTO board (member_id, title, content, reg_date, views, update_date) VALUES (2, 'title 2', 'content 2', sysdate, 2, sysdate);
+INSERT INTO board (member_id, title, content, reg_date, views, update_date) VALUES (3, 'title 3', 'content 3', sysdate, 3, sysdate);
+INSERT INTO board (member_id, title, content, reg_date, views, update_date) VALUES (4, 'title 4', 'content 4', sysdate, 4, sysdate);
+INSERT INTO board (member_id, title, content, reg_date, views, update_date) VALUES (5, 'title 5', 'content 5', sysdate, 5, sysdate);
