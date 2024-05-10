05109a52372585e86b0a9b0621dfbacab409ff7b
김준혁 <skdhd999@gmail.com>
2024-05-10 17:20:08 +0900

    Board Entity

--- board/src/main/java/com/board/domain/Board.java
+++ board/src/main/java/com/board/domain/Board.java
@@ -1,22 +1,33 @@
 package com.board.domain;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Board {
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long board_id;
+	private String member_id;
+	private String title;
+	private String content;
+	private int views;
 	
-	public Long getId() {
-		return id;
-	}
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime reg_date;
 	
-	public void setId(Long id) {
-		this.id = id;
-	}
-
+	@LastModifiedDate
+	private LocalDateTime update_Date;
+	
 }
